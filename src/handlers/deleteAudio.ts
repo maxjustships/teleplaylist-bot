9b4014cb4f7e5ea502b66e1747c6825aeb5acb87
Max <55242038+maxjustships@users.noreply.github.com>
2021-12-20 09:54:56 +0700

    handling error of playlist back if the audio message was removed

--- src/handlers/deleteAudio.ts
+++ src/handlers/deleteAudio.ts
@@ -3,7 +3,10 @@ import Context from '@/models/Context'
 export default function deleteAudio(ctx: Context) {
   return Promise.all(
     ctx.dbuser.playlists[ctx.dbuser.selectedPlaylist].audio.map(
-      ({ messageId }) => ctx.api.deleteMessage(ctx.chat.id, messageId)
+      ({ messageId }) =>
+        ctx.api.deleteMessage(ctx.chat.id, messageId).catch(() => {
+          // user removed the song on their own. do nothing, yet
+        })
     )
   )
 }
