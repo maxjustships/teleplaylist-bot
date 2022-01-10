67f425be328867ff86583537fc0c0d300e3bbe56
Max <55242038+maxjustships@users.noreply.github.com>
2022-01-10 15:06:26 +0700

    added loading for playlist closing

--- src/handlers/handlePlaylistBack.ts
+++ src/handlers/handlePlaylistBack.ts
@@ -1,8 +1,13 @@
 import Context from '@/models/Context'
 import deleteAudio from '@/handlers/deleteAudio'
+import getLoadingKeyboard from '@/helpers/loadingKeyboard'
 import sendMenu from '@/handlers/handleMenu'
 
 export default async function handlePlaylistBack(ctx: Context) {
+  const { message_id } = await ctx.reply(ctx.i18n.t('loading'), {
+    reply_markup: getLoadingKeyboard(ctx),
+  })
+  ctx.dbuser.lastBotMessages.push(message_id)
   await deleteAudio(ctx)
 
   ctx.dbuser.selectedPlaylist = -1
